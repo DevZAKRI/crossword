f3075ca583790177e58cf6f2acab5b3e6c006302
mostafa zakri <mostafazakri3@gmail.com>
2024-12-24 19:01:07 +0100

    almost done v1

--- crosswordSolver.js
+++ crosswordSolver.js
@@ -1,141 +1,196 @@
-const puzzle = `..1.1..1...
-10000..1000
-..0.0..0...
-..1000000..
-..0.0..0...
-1000..10000
-..0.1..0...
-....0..0...
-..100000...
-....0..0...
-....0......`
-const words = [
-  'popcorn',
-  'fruit',
-  'flour',
-  'chicken',
-  'eggs',
-  'vegetables',
-  'pasta',
-  'pork',
-  'steak',
-  'cheese',
-]
+
+
 function crosswordSolver(emptyPuzzle, words) {
-    let lines = emptyPuzzle.split('\n')
-    let puzzle = lines.map((line) => line.split(''))
-    let start = getStartPosition(puzzle)
-    let place = []
-    for (const word of words) {
-        let placed = false;
-        for (let row = 0; row < puzzle.length; row++) {
-            for (let col = 0; col < puzzle[row].length; col++) {
-                if (canPlaceWord(puzzle, word, row, col, 'horizontal', start) && culculength(col, row, puzzle, word, 'horizontal')) {
-                    placeWord(puzzle, word, row, col, 'horizontal');
-                    placed = true;
-                     break
-                } 
-                if (canPlaceWord(puzzle, word, row, col, 'vertical', start)&& culculength(col, row, puzzle, word, 'vertical')) {
-                    placeWord(puzzle, word, row, col, 'vertical');
-                    placed = true;
-                    break;
-                }
-            }
-            if (placed) break;
-        }
-        // if (!placed) {
-        //     console.log('Error');
-        //     return;
-        // }
+    if (!validPuzzle(emptyPuzzle, words)) {
+        
+        return console.log("Error")
     }
-    let printPuzzle = ""
-    for (let line of puzzle) {
-        printPuzzle += line.join('') + '\n'
+    if (!validWords(words)) {
+        return console.log("Error")
     }
-    return printPuzzle
-    
+    let lines = emptyPuzzle.split('\n');
+    let puzzle = lines.map((line) => line.split(''));
+     console.log(puzzle)
+    let start = getStartPosition(puzzle);
+    console.log(start)
+
+    if (start.length != words.length) {
+        return console.log("Error")
+    }
+
+    let solved = initSolvePuzzle(puzzle, words, start)
+    if (!solved) {
+        return console.log("Error")
+    }
+    return console.log(solved.map(row => row.join('')).join('\n'))
 }
-function placeWord(puzzle, word, row, col, direction) {
-    if (direction == 'horizontal') {
-        for (let i = 0; i < word.length; i++) {
-            if (i == word.length-1 && i<puzzle[row].length && puzzle[row][col + i +1] == '0') {
-                return false
-            } else {
-                puzzle[row][col + i] = word[i]; 
+
+const solvePuzzle = (puzzle, positions, words, index, usedWords, solutions) => {
+
+    if (index == positions.length) {
+        solutions.push(puzzle.map(row => [...row]));
+        return null
+    }
+
+    const pos = positions[index];
+    
+    for (let word of words) {
+        if (!usedWords.has(word) && canPlaceWord(puzzle, pos, word)) {
+
+            const tempPuzzle = puzzle.map(row => [...row]);
+            placeWord(tempPuzzle, pos, word);
+            usedWords.add(word);
+            
+            const result = solvePuzzle(tempPuzzle, positions, words, index + 1, usedWords, solutions);
+            if (result) return result;
+            
+            usedWords.delete(word);
+        }
+    }
+    
+    return null;
+};
+
+function getStartPosition(puzzle) {
+    let start = [];
+    for (let x = 0; x < puzzle.length; x++) {
+        for (let y = 0; y < puzzle[x].length; y++) {
+            let char = parseInt(puzzle[x][y]);
+            if (char > 0) {
+                    if (validHorizontal(puzzle, x, y)) {
+                        start.push({direction: 'horizontal',row: x,col: y});
+                    }
+                    if (validVertical(puzzle, x, y)) {
+                        start.push({direction: 'vertical',row: x,col: y});
+                    }
             }
         }
-    } else if (direction == 'vertical') {
-        for (let i = 0; i < word.length; i++) {
-            if (i == word.length-1 && i<puzzle.length && puzzle[row+i+1][col] == '0') {
-                return false
-            } else {
-                puzzle[row+i][col] = word[i]; 
-            }
+    }
+    return start;
+}
+
+function initSolvePuzzle(puzzle, words, positions) {
+    const solvedPuzzle = puzzle.map(row => [...row]);
+    const solutions = []
+    return solvePuzzle(solvedPuzzle, positions, words, 0, new Set(), solutions);
+}
+
+function validPuzzle(puzzle, words) {
+    const validPattern = /^[.012\n]*$/;
+    let countStart = 0
+    for (let x = 0; x < puzzle.length; x++) {
+        for (let y = 0; y < puzzle[x].length; y++) {
+            let num = parseInt(puzzle[x][y])
+             if (!isNaN(num) && num>0) {
+                countStart += num
+             }
         }
+    }
+    
+    if (countStart != words.length) {
+        // console.log(countStart, "  ",words.length)
+        return false
+    }
+
+    if (validPattern.test(puzzle)) {
+        return true
+    }
+    // console.log("Hehelala")
+    return false
+}
+
+function validWords(words) {
+    if (!Array.isArray(words)) {
+        // console.log("Hehehehe")
+        return false
+    }
+    const uniqueWords = new Set(words)
+
+    if (uniqueWords.size != words.length) {
+        // console.log("lalalala")
+        return false
     }
     return true
 }
-function canPlaceWord(puzzle, word, row, col, direction, start) {
-                
-  /*   let [numW, row, col] = start;
-    console.log(puzzle[row].slice(col))
-    if (word.length <= puzzle[row].slice(col).length) {
-        console.log("word: ", word)
+
+
+function canPlaceWord(puzzle, position, word) {
+
+    if (position.direction === 'horizontal') {
+        if (word.length > puzzle[position.row].slice(position.col).length) return false;
         for (let i = 0; i < word.length; i++) {
-            if (puzzle[row][+col + i] == '.' && (puzzle[row][+col + i+1] != '.' && i< word.length)) {
-                return false;
-            } else {
-                console.log(word, "err")
-            }
-        }
-        return true;
-    }
-    if (row + word.length <= puzzle.length - row) {
-        for (let i = 0; i < word.length; i++) {
-            if (puzzle[+row + i][col] == '.') {
+            let char = puzzle[position.row][position.col+i]
+            if (isNaN(char) && char != word[i]) {
                 return false;
             }
         }
-        return true;
+    } else {
+        if (position.row + word.length > puzzle.length) return false;
+        for (let i = 0; i < word.length; i++) {
+            let char = puzzle[position.row + i][position.col]
+            if (isNaN(char) && char != word[i]) {
+                return false;
+            }
+        }
     }
-    return false; */
+
+    return true;
 }
-function culculength(col,row, puzzle,word,d) {
-    let c =0
-    if (d=="horizontal"){
-        for (let clum = col;clum < puzzle[row].length; clum++) {
-            if (puzzle[row][clum]!='.'){
-             c++
-            }else{
-             break
-            }
-             
+
+function placeWord(puzzle, position, word) {
+    if (position.direction === 'horizontal') {
+        for (let i = 0; i < word.length; i++) {
+            puzzle[position.row][position.col + i] = word[i];
         }
-    }else if (d =="vertical"){
-        for (let rows = row; rows < puzzle.length; clum++) {
-            if (puzzle[rows][col]!='.'){
-             c++
-            }else{
-             break
-            }
-             
+    } else {
+        for (let i = 0; i < word.length; i++) {
+            puzzle[position.row + i][position.col] = word[i];
         }
-    } 
-    if (word.length==c){
+    }
+}
+
+
+
+function validHorizontal(puzzle, x, y) {
+    if (y > 0 && puzzle[x][y-1] !== '.') return false;
+    if (y < puzzle[x].length - 1 && puzzle[x][y + 1] != '.') {
         return true
     }
     return false
 }
-function getStartPosition(puzzle) {
-    let start = []
-    let x = 0
-    for (let rows in puzzle) {
-        for (let cols in puzzle[rows]) {
-            if (puzzle[rows][cols] != '.' && puzzle[rows][cols] != '0') {
-                start.push([puzzle[rows][cols], rows, cols])
+
+function validVertical(puzzle, x, y) {
+    if (x > 0 && puzzle[x-1][y] !== '.') return false;
+    if (x < puzzle.length - 1 && puzzle[x + 1][y] != '.') {
+       return true
+    }
+    return false
+}
+
+function culculength(col, row, puzzle, word, direction) {
+    let c = 0;
+    if (direction === 'horizontal') {
+        for (let clum = col; clum < puzzle[row].length; clum++) {
+            if (puzzle[row][clum] !== '.') {
+                c++;
+            } else {
+                break;
+            }
+        }
+    } else if (direction === 'vertical') {
+        for (let rows = row; rows < puzzle.length; rows++) {
+            if (puzzle[rows][col] !== '.') {
+                c++;
+            } else {
+                break;
             }
         }
     }
-    return start
+    return word.length === c;
 }
-console.log(crosswordSolver(puzzle, words))
+
+
+const puzzle = '2000\n0...\n0...\n0...'
+const words = ['abba', 'assa']
+
+crosswordSolver(puzzle, words)
